0898134e01cc763b684305abcb1f1d4cdc7fdc7c
HONGHUI CHOI <hc7795@cs.utexas.edu>
2013-07-03 14:04:43 -0500

    left-shift, right-shift, plus done

---
+++ Integer.h
@@ -0,0 +1,563 @@
+// --------------------------
+// projects/integer/Integer.h
+// Copyright (C) 2013
+// Glenn P. Downing
+// --------------------------
+
+#ifndef Integer_h
+#define Integer_h
+
+// --------
+// includes
+// --------
+
+#include <cassert>   // assert
+#include <iostream>  // ostream
+#include <stdexcept> // invalid_argument
+#include <string>    // string
+#include <vector>    // vector
+using namespace std;
+// -----------------
+// shift_left_digits
+// -----------------
+
+/**
+ * @param b an iterator to the beginning of an input  sequence (inclusive)
+ * @param e an iterator to the end       of an input  sequence (exclusive)
+ * @param x an iterator to the beginning of an output sequence (inclusive)
+ * @return  an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the shift left of the input sequence into the output sequence
+ * ([b, e) << n) => x
+ */
+template <typename II, typename OI>
+OI shift_left_digits (II b, II e, int n, OI x) {
+    while (b != e) {
+        *x = *b;
+    	++x;
+    	++b; 
+    }
+    while (n) {
+        *x = 0;
+        ++x;
+        --n;
+    }
+    return x;}
+
+// ------------------
+// shift_right_digits
+// ------------------
+
+/**
+ * @param b an iterator to the beginning of an input  sequence (inclusive)
+ * @param e an iterator to the end       of an input  sequence (exclusive)
+ * @param x an iterator to the beginning of an output sequence (inclusive)
+ * @return  an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the shift right of the input sequence into the output sequence
+ * ([b, e) >> n) => x
+ */
+template <typename II, typename OI>
+OI shift_right_digits (II b, II e, int n, OI x) {
+    while (b != (e-n)) {
+        *x = *b;
+        ++x;
+        ++b;
+    }
+    return x;}
+
+// -----------
+// plus_digits
+// -----------
+
+/**
+ * @param b  an iterator to the beginning of an input  sequence (inclusive)
+ * @param e  an iterator to the end       of an input  sequence (exclusive)
+ * @param b2 an iterator to the beginning of an input  sequence (inclusive)
+ * @param e2 an iterator to the end       of an input  sequence (exclusive)
+ * @param x  an iterator to the beginning of an output sequence (inclusive)
+ * @return   an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the sum of the two input sequences into the output sequence
+ * ([b1, e1) + [b2, e2)) => x
+ */
+template <typename II1, typename II2, typename OI>
+OI plus_digits (II1 b1, II1 e1, II2 b2, II2 e2, OI x) {
+	vector<int> vec;
+	int count = 0;
+	while (b1 != e1) {
+	    vec.push_back(*b1 + *b2);
+		b1++;
+		b2++;
+		count++;
+	}
+	vector<int>::reverse_iterator iter;
+	vector<int> temp;
+	bool add = false;
+	for(iter = vec.rbegin(); iter != vec.rend(); ++iter) {
+		if(add) {
+			*iter += 1;
+			add = false;
+		}
+		if(*iter >= 10) {
+			*iter = *iter - 10;
+			add = true;	
+		}
+		temp.push_back(*iter);
+	}
+	int i = 1;
+	for (vector<int>::reverse_iterator it = temp.rbegin(); it != temp.rend(); ++it) {
+		*x = *it;
+		if (i < count)
+		    ++x;
+	}
+    return x;}
+
+// ------------
+// minus_digits
+// ------------
+
+/**
+ * @param b  an iterator to the beginning of an input  sequence (inclusive)
+ * @param e  an iterator to the end       of an input  sequence (exclusive)
+ * @param b2 an iterator to the beginning of an input  sequence (inclusive)
+ * @param e2 an iterator to the end       of an input  sequence (exclusive)
+ * @param x  an iterator to the beginning of an output sequence (inclusive)
+ * @return   an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the difference of the two input sequences into the output sequence
+ * ([b1, e1) - [b2, e2)) => x
+ */
+ /*
+  void test_minus_digits () {
+        const int a[] = {8, 0, 1};
+        const int b[] = {5, 6, 7};
+        const int c[] = {2, 3, 4};
+              int x[10];
+        const int* p = minus_digits(a, a + 3, b, b + 3, x);
+        CPPUNIT_ASSERT(p - x == 3);
+        CPPUNIT_ASSERT(std::equal(const_cast<const int*>(x), p, c));}
+ */
+template <typename II1, typename II2, typename OI>
+OI minus_digits (II1 b1, II1 e1, II2 b2, II2 e2, OI x) {
+	//e1 -= 1;
+	//e2 -= 1;
+	//while (e1 != )    
+    return x;}
+
+// -----------------
+// multiplies_digits
+// -----------------
+
+/**
+ * @param b  an iterator to the beginning of an input  sequence (inclusive)
+ * @param e  an iterator to the end       of an input  sequence (exclusive)
+ * @param b2 an iterator to the beginning of an input  sequence (inclusive)
+ * @param e2 an iterator to the end       of an input  sequence (exclusive)
+ * @param x  an iterator to the beginning of an output sequence (inclusive)
+ * @return   an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the product of the two input sequences into the output sequence
+ * ([b1, e1) * [b2, e2)) => x
+ */
+template <typename II1, typename II2, typename OI>
+OI multiplies_digits (II1 b1, II1 e1, II2 b2, II2 e2, OI x) {
+    // <your code>
+    return x;}
+
+// --------------
+// divides_digits
+// --------------
+
+/**
+ * @param b  an iterator to the beginning of an input  sequence (inclusive)
+ * @param e  an iterator to the end       of an input  sequence (exclusive)
+ * @param b2 an iterator to the beginning of an input  sequence (inclusive)
+ * @param e2 an iterator to the end       of an input  sequence (exclusive)
+ * @param x  an iterator to the beginning of an output sequence (inclusive)
+ * @return   an iterator to the end       of an output sequence (exclusive)
+ * the sequences are of decimal digits
+ * output the division of the two input sequences into the output sequence
+ * ([b1, e1) / [b2, e2)) => x
+ */
+template <typename II1, typename II2, typename OI>
+OI divides_digits (II1 b1, II1 e1, II2 b2, II2 e2, OI x) {
+    // <your code>
+    return x;}
+
+// -------
+// Integer
+// -------
+
+template < typename T, typename C = std::vector<T> >
+class Integer {
+    // -----------
+    // operator ==
+    // -----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator == (const Integer& lhs, const Integer& rhs) {
+        // <your code>
+        return false;}
+
+    // -----------
+    // operator !=
+    // -----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator != (const Integer& lhs, const Integer& rhs) {
+        return !(lhs == rhs);}
+
+    // ----------
+    // operator <
+    // ----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator < (const Integer& lhs, const Integer& rhs) {
+        // <your code>
+        return false;}
+
+    // -----------
+    // operator <=
+    // -----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator <= (const Integer& lhs, const Integer& rhs) {
+        return !(rhs < lhs);}
+
+    // ----------
+    // operator >
+    // ----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator > (const Integer& lhs, const Integer& rhs) {
+        return (rhs < lhs);}
+
+    // -----------
+    // operator >=
+    // -----------
+
+    /**
+     * <your documentation>
+     */
+    friend bool operator >= (const Integer& lhs, const Integer& rhs) {
+        return !(lhs < rhs);}
+
+    // ----------
+    // operator +
+    // ----------
+
+    /**
+     * <your documentation>
+     */
+    friend Integer operator + (Integer lhs, const Integer& rhs) {
+        return lhs += rhs;}
+
+    // ----------
+    // operator -
+    // ----------
+
+    /**
+     * <your documentation>
+     */
+    friend Integer operator - (Integer lhs, const Integer& rhs) {
+        return lhs -= rhs;}
+
+    // ----------
+    // operator *
+    // ----------
+
+    /**
+     * <your documentation>
+     */
+    friend Integer operator * (Integer lhs, const Integer& rhs) {
+        return lhs *= rhs;}
+
+    // ----------
+    // operator /
+    // ----------
+
+    /**
+     * <your documentation>
+     * @throws invalid_argument if (rhs == 0)
+     */
+    friend Integer operator / (Integer lhs, const Integer& rhs) {
+        return lhs /= rhs;}
+
+    // ----------
+    // operator %
+    // ----------
+
+    /**
+     * <your documentation>
+     * @throws invalid_argument if (rhs <= 0)
+     */
+    friend Integer operator % (Integer lhs, const Integer& rhs) {
+        return lhs %= rhs;}
+
+    // -----------
+    // operator <<
+    // -----------
+
+    /**
+     * <your documentation>
+     * @throws invalid_argument if (rhs < 0)
+     */
+    friend Integer operator << (Integer lhs, int rhs) {
+        return lhs <<= rhs;}
+
+    // -----------
+    // operator >>
+    // -----------
+
+    /**
+     * <your documentation>
+     * @throws invalid_argument if (rhs < 0)
+     */
+    friend Integer operator >> (Integer lhs, int rhs) {
+        return lhs >>= rhs;}
+
+    // -----------
+    // operator <<
+    // -----------
+
+    /**
+     * <your documentation>
+     */
+    friend std::ostream& operator << (std::ostream& lhs, const Integer& rhs) {
+        // <your code>
+        return lhs << "0";}
+
+    // ---
+    // abs
+    // ---
+
+    /**
+     * absolute value
+     * does NOT modify the argument
+     * <your documentation>
+     */
+    friend Integer abs (Integer x) {
+        return x.abs();}
+
+    // ---
+    // pow
+    // ---
+
+    /**
+     * power
+     * does NOT modify the argument
+     * <your documentation>
+     * @throws invalid_argument if (x == 0) && (e == 0)
+     * @throws invalid_argument if (e < 0)
+     */
+    friend Integer pow (Integer x, int e) {
+        return x.pow(e);}
+
+    private:
+        // ----
+        // data
+        // ----
+
+        // <your data>
+
+    private:
+        // -----
+        // valid
+        // -----
+
+        bool valid () const {
+            // <your code>
+            return true;}
+
+    public:
+        // ------------
+        // constructors
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        Integer (int value) {
+            // <your code>
+            assert(valid());}
+
+        /**
+         * <your documentation>
+         * @throws invalid_argument if value is not a valid representation of an Integer
+         */
+        explicit Integer (const std::string& value) {
+            // <your code>
+            if (!valid())
+                throw std::invalid_argument("Integer::Integer()");}
+
+        // Default copy, destructor, and copy assignment.
+        // Integer (const Integer&);
+        // ~Integer ();
+        // Integer& operator = (const Integer&);
+
+        // ----------
+        // operator -
+        // ----------
+
+        /**
+         * <your documentation>
+         */
+        Integer operator - () const {
+            // <your code>
+            return Integer(0);}
+
+        // -----------
+        // operator ++
+        // -----------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator ++ () {
+            *this += 1;
+            return *this;}
+
+        /**
+         * <your documentation>
+         */
+        Integer operator ++ (int) {
+            Integer x = *this;
+            ++(*this);
+            return x;}
+
+        // -----------
+        // operator --
+        // -----------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator -- () {
+            *this -= 1;
+            return *this;}
+
+        /**
+         * <your documentation>
+         */
+        Integer operator -- (int) {
+            Integer x = *this;
+            --(*this);
+            return x;}
+
+        // -----------
+        // operator +=
+        // -----------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator += (const Integer& rhs) {
+            // <your code>
+            return *this;}
+
+        // -----------
+        // operator -=
+        // -----------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator -= (const Integer& rhs) {
+            // <your code>
+            return *this;}
+
+        // -----------
+        // operator *=
+        // -----------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator *= (const Integer& rhs) {
+            // <your code>
+            return *this;}
+
+        // -----------
+        // operator /=
+        // -----------
+
+        /**
+         * <your documentation>
+         * @throws invalid_argument if (rhs == 0)
+         */
+        Integer& operator /= (const Integer& rhs) {
+            // <your code>
+            return *this;}
+
+        // -----------
+        // operator %=
+        // -----------
+
+        /**
+         * <your documentation>
+         * @throws invalid_argument if (rhs <= 0)
+         */
+        Integer& operator %= (const Integer& rhs) {
+            // <your code>
+            return *this;}
+
+        // ------------
+        // operator <<=
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator <<= (int n) {
+            // <your code>
+            return *this;}
+
+        // ------------
+        // operator >>=
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        Integer& operator >>= (int n) {
+            // <your code>
+            return *this;}
+
+        // ---
+        // abs
+        // ---
+
+        /**
+         * absolute value
+         * <your documentation>
+         */
+        Integer& abs () {
+            // <your code>
+            return *this;}
+
+        // ---
+        // pow
+        // ---
+
+        /**
+         * power
+         * <your documentation>
+         * @throws invalid_argument if (this == 0) && (e == 0)
+         * @throws invalid_argument if (e < 0)
+         */
+        Integer& pow (int e) {
+            // <your code>
+            return *this;}};
+
+#endif // Integer_h
